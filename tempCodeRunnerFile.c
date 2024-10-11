1193a1552db24ef9afccefb7bc30970d0e90d12e
MonitoringLizard <raptop.versao.email@gmail.com>
2024-10-11 12:23:19 -0300

    Added UserInput.c, talking about, well user input

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,1 @@
+fgets(txt, 25, stdin);
---
+++ txtrpg.c
@@ -0,0 +1,10 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char select[25];
+    printf("\tAverage Text RPG\nstart\nexit\nwrite what you want =>");
+    scanf("%s", select);
+    return 0;
+}
