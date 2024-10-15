77dd97e2f846407f8749fddd9027556a860c7ddb
MonitoringLizard <raptop.versao.email@gmail.com>
2024-10-15 16:19:38 -0300

    Updated Switch.c

--- Cfiles/Switch.c
+++ Cfiles/Switch.c
@@ -5,11 +5,22 @@ int main()
     char letter;
     printf("Choose a letter from \'a\' to \'d\'\n");
     scanf("%c", &letter);
+
+    /*
+    "switch" is used as a more efficient way to test a value multiples times and find something thats is true,
+    as you can see we use switch(the variable or expression), and next we have "case" and a value,
+    it will test if the expression is equal to that value, if true, it will realize everything until it hits "break",
+    in case if its false, it will automatically jump to the next "case" 
+    */
     switch (letter)
     {
     case 'a':
+    /*  ^
+        |        
+    here it will test if the var "letter" has it value equal to 'a'*/
         printf("Allosaur is now your dinosaur\n");
         break;
+        //the "break;" is where, if true, were the program will stop and jump the switch to whats next in line
     case 'b':
         printf("Baryonix is now your dinosaur\n");
         break;
@@ -19,6 +30,7 @@ int main()
     case 'd':
         printf("Dilophosaur is now your dinosaur\n");
         break;
+        // "default" will only happen when every case above is false and it dont need a "break;"
     default:
         printf("[INVALID NUMBER]");
     }
